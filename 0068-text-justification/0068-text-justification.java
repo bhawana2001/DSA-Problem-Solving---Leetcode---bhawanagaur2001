473e07045f0a479e69816063d1dad693770d730f
Bhawana Gaur <gaur.bhawana2019@gmail.com>
2023-10-27 12:06:03 +0530

    Time: 0 ms (100%), Space: 40.4 MB (91.3%) - LeetHub

--- 0068-text-justification/0068-text-justification.java
+++ 0068-text-justification/0068-text-justification.java
@@ -1,42 +1,42 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         int n=words.length;
-        List<String> ans=new ArrayList<>();
         int i=0;
+        List<String> ans=new ArrayList<>();
         while(i<n){
-            int wc=words[i].length();
+            int lc=words[i].length();
             int j=i+1;
             int can=0;
-            while(j<n && wc+can+words[j].length()+1<=maxWidth){
-                wc+=words[j].length();
-                can++;
+            while(j<n && lc+can+words[j].length()+1<=maxWidth){
+                lc+=words[j].length();  //count total letters withing range
                 j++;
+                can++;                  //words included in current line
             }
-            int vac=maxWidth-wc;
-            // int can=j-i-1;
-            int atleast=can==0?0:vac/can;
-            int extra=can==0?0:vac%can;
-
-            if(j==words.length){
-                atleast=1;
-                extra=0;
+            int vac=maxWidth-lc;    //remaining vacancy after letter count
+            int atleastSpace=can==0?0:vac/can;
+            int extraSpace=can==0?0:vac%can;
+            if(j==n){
+                atleastSpace=1;
+                extraSpace=0;
             }
             StringBuilder sb=new StringBuilder();
             for(int k=i;k<j;k++){
                 sb.append(words[k]);
                 if(k==j-1) break;
-                for(int e=0;e<atleast;e++) sb.append(" ");
-                if(extra>0){
-                    sb.append(" ");
-                    extra--;
+                for(int space=0;space<atleastSpace;space++){
+                    sb.append(" ");     //fill spaces that is at least required 
+                }
+                if(extraSpace>0){
+                    sb.append(" ");     //fill spaces if there will be extra spaces required
+                    extraSpace--;
                 }
             }
-        while(sb.length()<maxWidth){
-        sb.append(" ");
-             }
-         ans.add(sb.toString());
-            i=j;
+            while(sb.length()<maxWidth){
+                sb.append(" ");     //that means we need to add few spaces to make it justified
             }
+            ans.add(sb.toString());
+            i=j;
+        }
         return ans;
     }
 }

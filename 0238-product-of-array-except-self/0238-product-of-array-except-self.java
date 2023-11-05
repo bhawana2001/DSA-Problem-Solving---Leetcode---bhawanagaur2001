238a9f25fb6219a339fedce545cbc376975eea9e
Bhawana Gaur <gaur.bhawana2019@gmail.com>
2023-11-05 18:28:01 +0530

    Time: 2 ms (65.48%), Space: 53.9 MB (13.15%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,35 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-      int[] ps=new int[nums.length];
-      int[] sp=new int[nums.length];
-      int[] res=new int[nums.length];
-      for(int i=0;i<nums.length;i++){
-          if(i==0){
-              ps[i]=1;
-          }else{
-          ps[i]=ps[i-1]*nums[i-1];
-          }
-      }
-      for(int i=nums.length-1;i>=0;i--){
-          if(i==nums.length-1){
-              sp[i]=1;
-          }else{
-          sp[i]=sp[i+1]*nums[i+1];
-          }
-      }
-      for(int i=0;i<nums.length;i++){
-          res[i]=ps[i]*sp[i];
-      }
-      return res;
+    public int[] productExceptSelf(int[] nums){
+        int n=nums.length;
+        int[] preProduct=new int[n];
+        int[] postProduct=new int[n];
+        int[] result=new int[n];
+        int pre=1;
+        int post=1;
+        // fill pre product array
+        for(int i=0;i<n;i++){
+            
+            if(i==0){
+                preProduct[i]=pre;
+               }else{
+           pre*=nums[i-1];
+           preProduct[i]=pre;
+               }
+        }
+        // fill post product array
+        for(int i=n-1;i>=0;i--){
+            if(i==n-1){
+                postProduct[i]=1;
+            }else{
+            post*=nums[i+1];
+            postProduct[i]=post;
+            }
+        }
+        //compute result array
+        for(int i=0;i<n;i++){
+        result[i]=preProduct[i]*postProduct[i];
+        }
+        return result;
     }
+
 }

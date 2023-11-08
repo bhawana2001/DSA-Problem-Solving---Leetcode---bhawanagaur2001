a19120ef47c822287536c277d8ff08f420e7b8ad
Bhawana Gaur <gaur.bhawana2019@gmail.com>
2023-11-08 08:50:03 +0530

    Time: 0 ms (100%), Space: 40.9 MB (90.16%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,22 +1,19 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        //Transpose the matrix
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+    public void rotate(int[][] matrix){
+        for(int row=0;row<matrix.length;row++){
+            for(int col=row;col<matrix[0].length;col++){
+                int temp=matrix[row][col];
+                matrix[row][col]=matrix[col][row];
+                matrix[col][row]=temp;
             }
         }
-
-        //reverse rows
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix.length/2;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[i][matrix.length-1-j];
-                matrix[i][matrix.length-1-j]=temp;
+        for(int row=0;row<matrix.length;row++){
+            for(int col=0;col<matrix[0].length/2;col++){
+                int temp=matrix[row][col];
+                matrix[row][col]=matrix[row][matrix.length-(col+1)];
+                matrix[row][matrix.length-(col+1)]=temp;
             }
         }
-
     }
 }
+

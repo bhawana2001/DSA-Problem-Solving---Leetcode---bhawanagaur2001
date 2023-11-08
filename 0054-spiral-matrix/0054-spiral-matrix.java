79b25c6e5da0185069c302d250e1b7ef6a5e0ac0
Bhawana Gaur <gaur.bhawana2019@gmail.com>
2023-11-08 08:40:37 +0530

    Time: 0 ms (100%), Space: 39.9 MB (99.15%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,43 +1,42 @@
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res=new ArrayList<>();
-        int top=0,down=matrix.length-1;
-        int left=0,right=matrix[0].length-1;
-        int dir=0;
-    
-        while(top<=down && left<=right){
-            // dir=0 means left to right
-            if(dir==0){
-               for(int i=left;i<=right;i++){
-                   res.add(matrix[top][i]);
-               }
-            top++;
+    public List<Integer> spiralOrder(int[][] matrix){
+        int left=0, right=matrix[0].length-1;
+        int top=0, bottom=matrix.length-1;
+        int direction=0;
+        List<Integer> ans=new ArrayList<>();
+        while(left<=right && top<=bottom){
+            // Direction 0 – Left to Right 
+            if(direction==0){
+                for(int i=left;i<=right;i++){
+                   ans.add(matrix[top][i]);
+                }
+                top++;
             }
-            // dir=1 means top to bottom
-            else if(dir==1){
-                for(int i=top;i<=down;i++){
-                    res.add(matrix[i][right]);
+            // Direction 1 — Top to Bottom
+            if(direction==1){
+                for(int i=top;i<=bottom;i++){
+                    ans.add(matrix[i][right]);
+                }
+                right--;
             }
-            right--;
-            }
-            // dir=2 means right to left
-           else if(dir==2){
+            // Direction 2 — Right to Left
+            if(direction==2){
                 for(int i=right;i>=left;i--){
-                    res.add(matrix[down][i]);
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
             }
-            down--;
-           }
-            // dir=3 means down to top
-            else if(dir==3){
-                for(int i=down;i>=top;i--){
-                    res.add(matrix[i][left]);
+            // Direction 3 — Bottom to Top
+            if(direction==3){
+                for(int i=bottom;i>=top;i--){
+                    ans.add(matrix[i][left]);
+                }
+                left++;
             }
-            left++;
-            }
-            
-            //update direction
-            dir=(dir+1)%4;
+            // Direction updates
+            direction=(direction+1)%4;
         }
-        return res;
+        return ans;
     }
+
 }
